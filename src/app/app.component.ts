74db57d13c3727e8f3feeac4e28e00f0270a0aa3
salmank07 <mohamedsalmankhan509@gmail.com>
2022-02-16 20:27:57 +0530

    day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alinfra';
+
+  
+
+recentWorks = [
+  {src:"../assets/images/recw1.jpeg"},
+  {src:"../assets/images/recw2.jpeg"},
+  {src:"../assets/images/recw3.jpeg"},
+  {src:"../assets/images/recw4.jpeg"},
+  {src:"../assets/images/recw5.jpeg"},
+  {src:"../assets/images/recw6.jpeg"},
+  {src:"../assets/images/recw7.jpeg"},
+  {src:"../assets/images/recw8.jpeg"},
+  {src:"../assets/images/recw9.jpeg"}
+]
+
+
 }
