3c6bb50191992a4ba921db0141f933fc1f96c10e
salmank07 <mohamedsalmankhan509@gmail.com>
2022-02-17 23:37:36 +0530

    day2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Alinfra';
+  collection: any;
+  list = true;
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    this.response();
+  }
+
+  recentWorks = [
+    { src: "../assets/images/recw1.jpeg", id: "1" },
+    { src: "../assets/images/recw2.jpeg", id: "2", t: "2" },
+    { src: "../assets/images/recw3.jpeg", id: "3", t: "3" },
+    { src: "../assets/images/recw4.jpeg", id: "4", t: "2" },
+    { src: "../assets/images/recw5.jpeg", id: "5", t: "2" },
+    { src: "../assets/images/recw6.jpeg", id: "6" },
+    { src: "../assets/images/recw7.jpeg", id: "7", t: "4" },
+    { src: "../assets/images/recw8.jpeg", id: "8", t: "2" },
+    { src: "../assets/images/recw9.jpeg", id: "9" }
+  ]
+  response() {
+    if (window.innerWidth <= 500) {
+      this.list = true;
+    }
+    else {
+      this.list = false;
+    }
+  }
 
   
-
-recentWorks = [
-  {src:"../assets/images/recw1.jpeg"},
-  {src:"../assets/images/recw2.jpeg"},
-  {src:"../assets/images/recw3.jpeg"},
-  {src:"../assets/images/recw4.jpeg"},
-  {src:"../assets/images/recw5.jpeg"},
-  {src:"../assets/images/recw6.jpeg"},
-  {src:"../assets/images/recw7.jpeg"},
-  {src:"../assets/images/recw8.jpeg"},
-  {src:"../assets/images/recw9.jpeg"}
-]
-
-
 }
